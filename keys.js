afb7a70e5a2ae19a86173044210df0317c3e411b
Yoav Saroya <stankovic100@gmail.com>
2017-06-19 07:55:18 +0300

    ready to first submit

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MLAB_KEY: 'mongodb://db_user:db_pass@ds121192.mlab.com:21192/popzit_db',
+    LASTFM_KEY: 'http://ws.audioscrobbler.com/2.0/?api_key=19895546ebffc89d327e1b8f2c89f690&format=json',
+    YOUTUBE_KEY: 'AIzaSyBMMXiB1gUamu2rvcBQvc36BqowskPpM4E'
+};
